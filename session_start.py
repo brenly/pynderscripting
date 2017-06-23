d3b48166aded00a8c8a66fc9b72cf7efd236f212
brenly <brenlyd@gmail.com>
2017-06-23 14:46:49 -0500

    immediate cleanup

--- session_start.py
+++ session_start.py
@@ -1,15 +1,19 @@
 import pynder
 import tokengetter
-
+#facebook authentication info will be in auth.info as follows:
+#line 1: facebook id number
+#line 2: email
+#line 3: password
 
 #auth_info = open("auth.info").readlines()
 #[l.strip('\n\r') for l in auth_info]
 
 fb_access_token = (tokengetter.get_access_token(tokengetter.auth_info[1], tokengetter.auth_info[2]))
+#output for testing verification purposes only
 #print(fb_access_token)
 session = pynder.Session(tokengetter.auth_info[0], fb_access_token)
 
 #session.matches() # get users you have already been matched with
-#session.update_location("33.2148", "97.1331") # updates latitude and longitude for your profile
+#session.update_location(latitude, longitude) # updates latitude and longitude for your profile
 #session.profile  # your profile. If you update its attributes they will be updated on Tinder.
 #users = session.nearby_users() # returns a iterable of users nearby
